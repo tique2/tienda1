d394842c306e481f5b14849231a925501d8c4a61
princesitasofia <princesitasofia@localhost.localdomain>
2023-04-22 02:46:20 -0400

    Guardando cambios en DbHelperproducto.java

--- parcial1/app/src/main/java/com/example/parcial1/db/DbHelperproducto.java
+++ parcial1/app/src/main/java/com/example/parcial1/db/DbHelperproducto.java
@@ -3,26 +3,23 @@ package com.example.parcial1.db;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.annotation.Nullable;
 
-public class DbHelperproducto extends SQLiteOpenHelper {
-    private static final int DATAVASE_VERSION=1;
-    private static final String DATAVASE_NAME="producto.db";
-    public static final String DATAVASE_TABLE_producto="producto";
-    public DbHelperproducto(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATAVASE_NAME, null, DATAVASE_VERSION);
+public class DbHelperProducto extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "parcial1.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DbHelperProducto(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase dbproducto) {
-        dbproducto.execSQL("create table "+DATAVASE_TABLE_producto+"(" +
-                "id integer primary key autoincrement," +
-                "nombre text not null," +
-                "cantidad text not null)");
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE productos (_id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, cantidad TEXT);");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Aquí puedes agregar código para actualizar la base de datos
     }
 }
